852183555f553073ebb7e8729327c5eba2526548
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-02-15 15:56:28 -0300

    ChessException and ChessPosition - improved piece placement

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
-import chess.Color;
 
 import chess.pieces.Bishop;
 import chess.pieces.King;
@@ -32,53 +30,15 @@ public class ChessMatch {
         return mat;
     }
 
-    private void initialSetup() {
-        // White Pieces
-        board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 2));
-        board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 5));
-
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
-
-        board.placePiece(new Knight(board, Color.WHITE), new Position(7, 1));
-        board.placePiece(new Knight(board, Color.WHITE), new Position(7, 6));
-
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 0));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 1));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 2));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 3));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 4));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 5));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 6));
-        board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 7));
-
-        board.placePiece(new Queen(board, Color.WHITE), new Position(7, 3));
-
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
-
-
-        // Black Pieces
-        board.placePiece(new Bishop(board, Color.BLACK), new Position(0, 2));
-        board.placePiece(new Bishop(board, Color.BLACK), new Position(0, 5));
-
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-
-        board.placePiece(new Knight(board, Color.BLACK), new Position(0, 1));
-        board.placePiece(new Knight(board, Color.BLACK), new Position(0, 6));
-
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 0));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 1));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 2));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 3));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 5));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 4));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 6));
-        board.placePiece(new Pawn(board, Color.BLACK), new Position(1, 7));
-
-        board.placePiece(new Queen(board, Color.BLACK), new Position(0, 3));
-
-        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
+    private void initialSetup() {
+        // White Pieces
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+
+        // Black Pieces
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+    }
 }
