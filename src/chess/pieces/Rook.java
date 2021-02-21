5c26200a790c8795b9398bfa2e0be8d505933768
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-02-21 11:55:20 -0300

    feat: rook possible moves logic

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -17,8 +17,53 @@ public class Rook extends ChessPiece{
     }
 
     @Override
-    public boolean[][] possibleMoves() { //TODO delete generic possibleMoves(); implement specific logic;
+    public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // ABOVE ROOK
+        // setting first position value above rook
+        p.setValues(position.getRow() - 1, position.getColumn());
+        // while square above is empty
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true; // set square as possible move
+            p.setRow(p.getRow() - 1); // decrement to check next row above
+        }
+        // if position exists && it's an opponent's piece
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true; // true == it's a possible move
+        }
+
+        // BELOW ROOK
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // ROOK'S LEFT
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // ROOK'S RIGHT
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
         return mat;
     }
 
