b4d8fd1579f154c68bed7f9e418723274267dd4a
Thiago Amaral <it.eng.amaraldepaula@gmail.com>
2021-02-23 08:51:59 -0300

    feat: knight possible moves logic

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,9 +15,96 @@ public class Knight extends ChessPiece {
         return "N";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+
+        // if null || not your own piece
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() { //TODO delete generic possibleMoves(); implement specific logic;
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // See O'clock Reference
+        // commented by the end of this Class
+
+        // 1 o'clock
+        p.setValues(position.getRow() - 2, position.getColumn() +1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 2 o'clock
+        p.setValues(position.getRow() - 1, position.getColumn() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 4 o'clock
+        p.setValues(position.getRow() + 1, position.getColumn() + 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 5 o'clock
+        p.setValues(position.getRow() + 2, position.getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 7 o'clock
+        p.setValues(position.getRow() + 2, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 8 o'clock
+        p.setValues(position.getRow() + 1, position.getColumn() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 10 o'clock
+        p.setValues(position.getRow() - 1, position.getColumn() - 2);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // 11 o'clock
+        p.setValues(position.getRow() - 2, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
         return mat;
     }
 }
+/*
+
+O'clock Representation
+
+    -2 -1  0 +1 +2
+-2   -  z  -  p  -
+-1   w  -  -  -  q
+ 0   -  -  N  -  -
++1   y  -  -  -  r
++2   -  x  -  s  -
+
+
+        row column
+p = 1   -2  +1
+q = 2   -1  +2
+
+r = 4   +1  +2
+s = 5   +2  +1
+
+x = 7   +2  -1
+y = 8   +1  -2
+
+w = 10  -1  -2
+z = 11  -2  -1
+
+ */
